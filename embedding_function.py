da0222e01822eb894d40e7a8fb21416671d56006
adam-s10 <adam@steinmetz.com>
2025-04-14 17:23:42 +0100

    Created embedding_function.py and query_db.py, and modified populate_db.py
    embedding_function.py:
    - Abstracted GeminiEmbeddingFunction class to its own file for easy access by query_db.py and populate_db.py
    
    populate_db.py:
    - Removed GeminiEmbeddingFunction class
    - Removed code related to querying db
    
    query_db.py:
    - Added code related to querying db

---
+++ embedding_function.py
@@ -0,0 +1,32 @@
+from chromadb import EmbeddingFunction, Embeddings, Documents
+
+from google import genai
+from google.api_core import retry
+from google.genai import types
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+is_retriable = lambda e: (isinstance(e, genai.errors.APIError) and e.code in {429, 503})
+
+class GeminiEmbeddingFunction(EmbeddingFunction):
+    # Specify whether to generate embeddings for documents, or queries
+    document_mode = True
+
+    @retry.Retry(predicate=is_retriable)
+    def __call__(self, input_: Documents) -> Embeddings:
+        if self.document_mode:
+            embedding_task = 'retrieval_document'
+        else:
+            embedding_task = 'retrieval_query'
+
+        response = genai.Client(api_key=os.getenv('GOOGLE_API_KEY')).models.embed_content(
+            model='models/text-embedding-004',
+            contents=input_,
+            config=types.EmbedContentConfig(
+                task_type=embedding_task,
+            ),
+        )
+        return [em.values for em in response.embeddings]
